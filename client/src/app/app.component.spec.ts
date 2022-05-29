5adda39d2953c5fa96d60b0a16b6ac2d8231af04
Antonio Membrides Espinosa <tonykssa@gmail.com>
2022-05-28 22:02:28 -0400

    update front test

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('Angular / KsMf');
   });
 
   it('should render title', () => {
